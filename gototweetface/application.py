2e89dfa5a107663079264f3b7f9b65a49e3f46d9
Leandro Moreno <moreno.leandro.g@gmail.com>
2013-06-13 15:09:21 -0300

    Added base html and base handler, added empty css file for base html

--- gototweetface/application.py
+++ gototweetface/application.py
@@ -1,17 +1,22 @@
 __author__ = 'moreno'
 
+import os
 from gototweetface.handlers.facebook_handler import FacebookHandler
 from gototweetface.handlers.google_handler import GoogleHandler
 from gototweetface.handlers.twitter_handler import TwitterHandler
 from gototweetface.handlers.base_handler import BaseHandler
 from tornado import web
 
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 application = web.Application(
-    [
+    [   (r'/static',web.StaticFileHandler, {'path': os.path.join(BASE_DIR, "static")}),
+        (r'/favicon.ico', web.StaticFileHandler, {'path': os.path.join(BASE_DIR, "static")}),
         (r'.*/google', GoogleHandler),
         (r'.*/facebook', FacebookHandler),
         (r'.*/twitter', TwitterHandler),
-        (r'.*/(\w+?)/hello', BaseHandler)
+        (r'/', BaseHandler)
     ])
 
-application.settings["cookie_secret"] = "esteEsMiCookieMagico12345"
+application.settings.update({"cookie_secret": "esteEsMiCookieMagico12345"})
--- gototweetface/handlers/google_handler.py
+++ gototweetface/handlers/google_handler.py
@@ -13,13 +13,16 @@ class GoogleHandler(web.RequestHandler, auth.GoogleMixin):
             user = yield self.get_authenticated_user()
             # self.set_secure_cookie(user.get('claimed_id', None), )
               # Save the user with e.g. set_secure_cookie()
+
             message = "Hello " + user['name']
             message_mail = "Your e-mail is " + user['email']
             template = "<html> " + \
+                        "<head><title>Google</title></head>" + \
                         "<body>" + \
                         "<b>" + message + "</b> <br>" + \
                         message_mail + \
                        "</html>"
             self.write(template)
+            self.finish()
         else:
             self.authenticate_redirect()
